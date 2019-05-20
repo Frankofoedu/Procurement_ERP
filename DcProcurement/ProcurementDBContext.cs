d201ec8b94023b3e7ed48f3a1f311b4fef3c9fca
Okpara Obinna <obainoforgod@gmail.com>
2019-05-20 10:53:58 +0100

    Front End Design

---
+++ DcProcurement/ProcurementDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DcProcurement
+{
+    public class ProcurementDBContext : DbContext
+    {
+        public ProcurementDBContext(DbContextOptions options)
+           : base(options)
+        { }
+
+
+    }
+}
