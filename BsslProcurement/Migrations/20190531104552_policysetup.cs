5a2b8ec6f9ecf3f7bff6abc638f3329026141329
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-06-02 11:35:40 +0100

    add new table for policy setup

---
+++ BsslProcurement/Migrations/20190531104552_policysetup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BsslProcurement.Migrations
+{
+    public partial class policysetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PrequalificationPolicies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NoOfCategory = table.Column<int>(nullable: false),
+                    PrequalificationStartDate = table.Column<DateTime>(nullable: false),
+                    PrequalificationEndDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrequalificationPolicies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PrequalificationPolicies");
+        }
+    }
+}
