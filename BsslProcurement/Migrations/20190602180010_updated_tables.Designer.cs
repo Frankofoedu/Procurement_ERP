cc2fb4e7501bc5520908bf7fe49a3d7c5e03fef1
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-06-03 09:14:30 +0100

    new migrations

--- BsslProcurement/Migrations/20190602180010_updated_tables.Designer.cs
+++ BsslProcurement/Migrations/20190602180010_updated_tables.Designer.cs
@@ -4,14 +4,16 @@ using DcProcurement;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BsslProcurement.Migrations
 {
     [DbContext(typeof(ProcurementDBContext))]
-    partial class ProcurementDBContextModelSnapshot : ModelSnapshot
+    [Migration("20190602180010_updated_tables")]
+    partial class updated_tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
