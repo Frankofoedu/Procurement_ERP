9c7fbaf41f50b29709e9f4acb99e48c1dd28e4b6
Okpara Obinna <obainoforgod@gmail.com>
2019-06-05 13:41:46 +0100

    Before Identity

---
+++ DcProcurement/Workflow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DcProcurement
+{
+    class Workflow
+    {
+        public int Id { get; set; }
+        public int Step { get; set; }
+        public string Description { get; set; }
+        public bool ToPersonOrGroup { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
