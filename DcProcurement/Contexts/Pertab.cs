f1eae8218c8dc0b91d98b2b84ec842d911f827d0
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-08-05 09:07:11 +0100

    requisition update

---
+++ DcProcurement/Contexts/Pertab.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DcProcurement.Contexts
+{
+    public partial class Pertab
+    {
+        public decimal RecordId { get; set; }
+        public string Staffid { get; set; }
+        public string Confirmed { get; set; }
+        public DateTime? Employdate { get; set; }
+        public DateTime? Lastfurpay { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public string Birthplace { get; set; }
+        public decimal? Married { get; set; }
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public string Rescity { get; set; }
+        public string Homecity { get; set; }
+        public string Phoneno { get; set; }
+        public string Tribe { get; set; }
+        public decimal? Childno { get; set; }
+        public decimal? Spouseno { get; set; }
+        public string Spousename { get; set; }
+        public DateTime? Spousedob { get; set; }
+        public string Unioncode { get; set; }
+        public decimal? Relateno { get; set; }
+        public string Staffpic { get; set; }
+        public DateTime? Jobstart { get; set; }
+        public DateTime? Jobend { get; set; }
+        public decimal? Leavedays { get; set; }
+        public decimal? Leavebal { get; set; }
+        public string Loccode { get; set; }
+        public string Empplace { get; set; }
+        public bool? Retired { get; set; }
+        public string Section1 { get; set; }
+        public string Custcode { get; set; }
+        public DateTime? Dateconf { get; set; }
+        public decimal? Cat { get; set; }
+        public DateTime? Rdate { get; set; }
+        public string Compcode { get; set; }
+        public string Lga { get; set; }
+        public DateTime? Confirmduedate { get; set; }
+        public DateTime? Confirmdate { get; set; }
+        public string Cfmcomment { get; set; }
+        public string Jobtype { get; set; }
+        public string Jobcode { get; set; }
+        public string Jobdesc { get; set; }
+        public decimal? EmpType { get; set; }
+        public string EmpReason { get; set; }
+        public string EmgAddress { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public DateTime? Retireddate { get; set; }
+        public string Buddept { get; set; }
+        public string Office { get; set; }
+        public bool? StartInterdiction { get; set; }
+        public DateTime? IndStartDate { get; set; }
+        public bool? ResumeInterdiction { get; set; }
+        public DateTime? IndResumeDate { get; set; }
+        public int? ContributePen { get; set; }
+        public string UnitCode { get; set; }
+        public string SectionCode { get; set; }
+        public string ResCountry { get; set; }
+        public string ResState { get; set; }
+        public string ResZipCode { get; set; }
+        public string PermAddress { get; set; }
+        public string PermCountry { get; set; }
+        public string PermState { get; set; }
+        public string PermCity { get; set; }
+        public string PermZipCode { get; set; }
+        public string EmgCountry { get; set; }
+        public string EmgState { get; set; }
+        public string EmgCity { get; set; }
+        public string EmgZipCode { get; set; }
+        public bool? ExclContrPay { get; set; }
+        public DateTime? ReportTime { get; set; }
+        public string Retiredreason { get; set; }
+        public DateTime? SusTempStartDate { get; set; }
+        public DateTime? SusTempResumeDate { get; set; }
+        public DateTime? PayAddate { get; set; }
+    }
+}
