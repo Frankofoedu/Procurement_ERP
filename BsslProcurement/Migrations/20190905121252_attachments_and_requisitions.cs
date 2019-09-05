110eff189498b8ee6b7419a87215bca4be6cefd7
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-09-05 13:22:06 +0100

    requisition migrations

---
+++ BsslProcurement/Migrations/20190905121252_attachments_and_requisitions.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BsslProcurement.Migrations
+{
+    public partial class attachments_and_requisitions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requisitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PRNumber = table.Column<string>(nullable: false),
+                    ProcurementType = table.Column<string>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    DeliveryDate = table.Column<DateTime>(nullable: false),
+                    RequestionDepartment = table.Column<string>(nullable: false),
+                    RequiredAtDepartment = table.Column<string>(nullable: false),
+                    PreparedBy = table.Column<string>(nullable: false),
+                    PreparedByRank = table.Column<string>(nullable: true),
+                    PreparedFor = table.Column<string>(nullable: false),
+                    PreparedForRank = table.Column<string>(nullable: true),
+                    Purpose = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requisitions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attachments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FilePath = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()"),
+                    RequisitionId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attachments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attachments_Requisitions_RequisitionId",
+                        column: x => x.RequisitionId,
+                        principalTable: "Requisitions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RequisitionItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StoreItemCode = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Quantity = table.Column<string>(nullable: true),
+                    UnitOfMeasurement = table.Column<string>(nullable: true),
+                    VendorId = table.Column<string>(nullable: true),
+                    UnitPrice = table.Column<double>(nullable: false),
+                    RequisitionId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RequisitionItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RequisitionItems_Requisitions_RequisitionId",
+                        column: x => x.RequisitionId,
+                        principalTable: "Requisitions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RequisitionItems_AspNetUsers_VendorId",
+                        column: x => x.VendorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachments_RequisitionId",
+                table: "Attachments",
+                column: "RequisitionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequisitionItems_RequisitionId",
+                table: "RequisitionItems",
+                column: "RequisitionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequisitionItems_VendorId",
+                table: "RequisitionItems",
+                column: "VendorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attachments");
+
+            migrationBuilder.DropTable(
+                name: "RequisitionItems");
+
+            migrationBuilder.DropTable(
+                name: "Requisitions");
+        }
+    }
+}
