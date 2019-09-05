1d4f985cacb3c0ac971dac9235d8396efa2ea71e
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-09-05 13:22:26 +0100

    requisition models

--- DcProcurement/Requisition/Requisition.cs
+++ DcProcurement/Requisition/Requisition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DcProcurement
@@ -7,18 +8,25 @@ namespace DcProcurement
     public class Requisition
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Requisition Number is required")]
         public string PRNumber { get; set; }
-        public bool ProcurementType { get; set; }
+        [Required(ErrorMessage = "Select Type of procuremnet")]
+        public string ProcurementType { get; set; }
+        [Required(ErrorMessage ="Select Date")]
         public DateTime? Date { get; set; }
+        [Required(ErrorMessage = "Select Date")]
         public DateTime? DeliveryDate { get; set; }
+        [Required(ErrorMessage = "Select Department Requesting")]
         public string RequestionDepartment { get; set; }
-        public string RequiredAt { get; set; }
+        [Required(ErrorMessage = "Select Department Required At")]
+        public string RequiredAtDepartment { get; set; }
+        [Required(ErrorMessage = "Select Staff Prepared By")]
         public string PreparedBy { get; set; }
         public string PreparedByRank { get; set; }
+        [Required(ErrorMessage = "Select Staff Prepared For")]
         public string PreparedFor { get; set; }
         public string PreparedForRank { get; set; }
         public string Purpose { get; set; }
-
         public List<RequisitionItem> RequisitionItems { get; set; }
         public List<Attachment> Attachments { get; set; }
     }
