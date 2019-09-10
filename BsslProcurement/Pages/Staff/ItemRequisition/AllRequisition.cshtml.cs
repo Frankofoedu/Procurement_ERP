b6adf71c4311a811bce0b05d24d135dd2f65b31f
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-09-10 13:18:57 +0100

    updates ti item pricing and requisition pages

---
+++ BsslProcurement/Pages/Staff/ItemRequisition/AllRequisition.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DcProcurement.Contexts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BsslProcurement.Pages.Staff.ItemRequisition
+{
+    [Authorize]
+    public class AllRequisitionModel : PageModel
+    {
+        private readonly BSSLSYS_ITF_DEMOContext _context;
+        public AllRequisitionModel(BSSLSYS_ITF_DEMOContext context)
+        {
+            _context = context;
+        }
+        public string Message { get; set; }
+        public string Error { get; set; }
+
+        [BindProperty]
+        public List<Procreq2> ApprovedRequests { get; set; }
+
+        public Procreq2 NewRequisition { get; set; }
+        public async Task OnGetAsync()
+        {
+            ApprovedRequests = await _context.Procreq2.ToListAsync();
+        }
+    }
+}
