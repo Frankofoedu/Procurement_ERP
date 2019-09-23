fb59920d026739b801a05f6b3ece908a972c84a4
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-09-23 09:07:49 +0100

    added item pricing folder

---
+++ BsslProcurement/Pages/Staff/ItemRequisition/ItemPricing/AllRequisitionItemPricing.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DcProcurement;
+using DcProcurement.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BsslProcurement.Pages.Staff.ItemRequisition.ItemPricing
+{
+    public class AllRequisitionItemPricingModel : PageModel
+    {
+        private readonly ProcurementDBContext _context;
+        public AllRequisitionItemPricingModel(ProcurementDBContext context)
+        {
+            _context = context;
+        }
+
+
+        public string Message { get; set; }
+        public string Error { get; set; }
+
+
+
+        [BindProperty]
+        public List<Requisition> Requisitions { get; set; }
+        public async Task OnGetAsync()
+        {
+            //get all requisitions that havent been priced
+            Requisitions = await _context.Requisitions.Where(r => r.isPriced == false).Include(x => x.RequisitionItems).ToListAsync();
+        }
+    }
+}
---
+++ BsslProcurement/Pages/Staff/ItemRequisition/ItemPricing/RequisitionItemPricing.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DcProcurement;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace BsslProcurement.Pages.Staff.ItemRequisition
+{
+    [Authorize]
+    public class RequisitionItemPricingModel : PageModel
+    {
+        public readonly ProcurementDBContext context;
+
+
+        public string Message { get; set; }
+        public string Error { get; set; }
+        [BindProperty]
+        public DcProcurement.Requisition Requisition { get; set; }
+
+        private readonly DcProcurement.Contexts.BSSLSYS_ITF_DEMOContext bsslContext;
+        public RequisitionItemPricingModel(ProcurementDBContext _context, DcProcurement.Contexts.BSSLSYS_ITF_DEMOContext _bsslContext)
+        {
+            bsslContext = _bsslContext;
+            context = _context;
+        }
+
+        public void LoadData(int id)
+        {
+
+            Requisition = context.Requisitions.Include(x => x.Attachments).Include(y => y.RequisitionItems).Where(k => k.Id == id).FirstOrDefault();
+
+        }
+        public void OnGet(int id)
+        {
+            LoadData(id);
+        }
+
+
+        public void OnPost(int id)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+            }
+            LoadData(id);
+        }
+
+        public PartialViewResult OnGetItemPartial()
+        {
+
+           // get all vendor
+            var items = bsslContext.Stock.ToList();
+
+            return new PartialViewResult
+            {
+                ViewName = "Modals/_ItemLayout",
+                ViewData = new ViewDataDictionary<List<DcProcurement.Contexts.Stock>>(ViewData, items)
+            };
+        }
+
+        public PartialViewResult OnGetVendorPartial()
+        {
+
+            //get all vendor 
+            var vendors = context.Vendors.ToList();
+
+
+            return new PartialViewResult
+            {
+                ViewName = "_VendorLayoutModal",
+                ViewData = new ViewDataDictionary<List<VendorUser>>(ViewData, vendors)
+            };
+        }
+    }
+}
