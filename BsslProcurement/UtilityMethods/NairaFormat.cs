c92c37cb0a2a82ac91f3655a2e8a45979b8a4abd
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-09-23 09:05:27 +0100

    Added naira string formatter

---
+++ BsslProcurement/UtilityMethods/NairaFormat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BsslProcurement.UtilityMethods
+{
+    public static class NairaFormat
+    {
+        public static string GetNairaValue(decimal amount)
+        {
+            return string.Format(new CultureInfo("en-NG", false).NumberFormat, "{0:C}", amount);
+        }
+    }
+}
