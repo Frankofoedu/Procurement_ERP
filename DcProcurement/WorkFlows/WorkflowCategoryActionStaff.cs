2f68427c45a66e8d191894524366fc91d4d6b9ff
Obinna Okpara <obainoforgod@gmail.com>
2019-11-06 12:47:55 +0100

    Workflow category action staff setup

---
+++ DcProcurement/WorkFlows/WorkflowCategoryActionStaff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DcProcurement.WorkFlows
+{
+    class WorkflowCategoryActionStaff
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "This Field is required.")]
+        public int WorkflowCategoryId { get; set; }
+        public int WorkflowActionId { get; set; }
+        public string StaffId { get; set; }
+
+        public WorkflowType WorkflowType { get; set; }
+        public WorkflowAction WorkflowAction { get; set; }
+        public Staff Staff { get; set; }
+    }
+}
