0dfd1d0474a577abc00bdfee6ff482e2b83eb214
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-11-13 18:40:39 +0100

    added view models

---
+++ BsslProcurement/ViewModels/StaffLayoutModel.cs
@@ -0,0 +1,13 @@
+﻿using DcProcurement.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BsslProcurement.ViewModels
+{
+    public class StaffLayoutModel
+    {
+        public Stafftab Staff { get; set; }
+        public string Rank { get; set; }
+    }
+}
