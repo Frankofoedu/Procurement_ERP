647198569a170d34ac6ddc3591a42297fa37608e
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-11-27 17:55:28 +0100

    updated detail requisition

--- BsslProcurement/Pages/Staff/ItemRequisition/ProcCommencement/DetailRequisition.cshtml.cs
+++ BsslProcurement/Pages/Staff/ItemRequisition/ProcCommencement/DetailRequisition.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BsslProcurement.Interfaces;
+using BsslProcurement.Services;
 using BsslProcurement.ViewModels;
 using DcProcurement;
 using DcProcurement.Contexts;
@@ -17,10 +19,12 @@ namespace BsslProcurement.Pages.Staff.ItemRequisition
 
         private readonly ProcurementDBContext _context;
         private readonly BSSLSYS_ITF_DEMOContext _bsslContext;
-        public DetailRequisitionModel(ProcurementDBContext context, BSSLSYS_ITF_DEMOContext bsslContext)
+        private readonly IItemGridViewModelService _itemGridViewModelService;
+        public DetailRequisitionModel(ProcurementDBContext context, BSSLSYS_ITF_DEMOContext bsslContext, IItemGridViewModelService itemGridViewModelService)
         {
             _context = context;
             _bsslContext = bsslContext;
+            _itemGridViewModelService = itemGridViewModelService;
         }
         public string Message { get; set; }
         public string Error { get; set; }
@@ -34,12 +38,14 @@ namespace BsslProcurement.Pages.Staff.ItemRequisition
                 return NotFound();
             }
 
-            Requisition = await _context.Requisitions.Include(re => re.RequisitionItems).FirstOrDefaultAsync(x=> x.Id == id);
+            Requisition = await _context.Requisitions.FirstOrDefaultAsync(x=> x.Id == id);
+            ItemGridViewModels = await _itemGridViewModelService.GetItemsInRequisition(id.Value);
          //   ItemGridViewModels = Requisition.RequisitionItems.Select(x=> new ItemGridViewModel { Attachment = x.Attachment, RequisitionItem = x });
 
             if (Requisition == null)
             {
-                return NotFound();
+                Error = "No requisition Found";
+                return Page();
             }
 
             return Page();
