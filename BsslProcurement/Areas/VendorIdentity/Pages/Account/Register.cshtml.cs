df418b893bc1b7575531969e505116be711641d4
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-12-11 18:22:44 +0100

    vendor login system

---
+++ BsslProcurement/Areas/VendorIdentity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using DcProcurement;
+using DcProcurement.Contexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BsslProcurement.Areas.VendorIdentity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly BSSLSYS_ITF_DEMOContext _bsslContext;
+        //private readonly IEmailSender _emailSender;
+
+        public RegisterModel(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            ILogger<RegisterModel> logger,
+            BSSLSYS_ITF_DEMOContext bsslContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _bsslContext = bsslContext;
+            //_emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+
+            [Required]
+            [Display(Name = "Staff Code")]
+            public string StaffCode { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "Company Code")]
+            public string CompCode { get; set; } = "10";
+
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (!ModelState.IsValid) return Page();
+
+
+            var user = new Staff { UserName = Input.Email,
+                Email = Input.Email,
+                CreationDate =DateTime.Now,
+                Name = Input.Name,
+                StaffCode = Input.StaffCode
+
+            };
+
+            //add staff in the user acct table first
+            _bsslContext.Useracct.Add(new Useracct {Userid = Input.StaffCode, Pwd = Input.Password, Compcode = Input.CompCode, Procurement = true});
+            var addStaffResult = _bsslContext.SaveChanges();
+            if (addStaffResult != 1)
+            {
+                ModelState.AddModelError(string.Empty, "An error occured while adding staff account");
+                return Page();
+            }
+
+
+            //adds user to the identity tables
+            var result = await _userManager.CreateAsync(user, Input.Password);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User created a new account with password.");
+
+                //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                //var callbackUrl = Url.Page(
+                //    "/Account/ConfirmEmail",
+                //    pageHandler: null,
+                //    values: new { userId = user.Id, code = code },
+                //    protocol: Request.Scheme);
+
+                //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return LocalRedirect(returnUrl);
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
