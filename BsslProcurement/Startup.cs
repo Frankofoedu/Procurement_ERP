769b30ad5ca524e9b1b560c9a2077cb9b1f70f51
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-12-20 12:16:00 +0100

    fixed jquery bug. binds jquery to all staff buttons in modal

--- BsslProcurement/Startup.cs
+++ BsslProcurement/Startup.cs
@@ -51,7 +51,7 @@ namespace BsslProcurement
             string conn;
             if (Env.IsDevelopment())
             {
-                connection = @"Server=.\SQLEXPRESS;Database=DcProcurement;Trusted_Connection=True;ConnectRetryCount=0";
+                connection = @"Server=.\SQLEXPRESS;Database=DcProcurementTest;Trusted_Connection=True;ConnectRetryCount=0";
                 conn = @"Server =.\SQLEXPRESS; Database=BSSLSYS_ITF;Trusted_Connection=True;";
             }
             else
