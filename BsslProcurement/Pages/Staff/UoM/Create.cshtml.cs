dac23b138911fa95be1a5bf67e111be5aef938f2
Obinna Okpara <obainoforgod@gmail.com>
2020-01-08 09:56:50 +0100

    Unit of measurement pages

---
+++ BsslProcurement/Pages/Staff/UoM/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DcProcurement.Contexts;
+
+namespace BsslProcurement
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DcProcurement.Contexts.BSSLSYS_ITF_DEMOContext _context;
+
+        public CreateModel(DcProcurement.Contexts.BSSLSYS_ITF_DEMOContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public UnitOfMeasurement UnitOfMeasurement { get; set; }
+
+        public string Message { get; set; }
+        public string Error { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.UnitOfMeasurements.Add(UnitOfMeasurement);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ BsslProcurement/Pages/Staff/UoM/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DcProcurement.Contexts;
+
+namespace BsslProcurement
+{
+    public class EditModel : PageModel
+    {
+        private readonly DcProcurement.Contexts.BSSLSYS_ITF_DEMOContext _context;
+
+        public EditModel(DcProcurement.Contexts.BSSLSYS_ITF_DEMOContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public UnitOfMeasurement UnitOfMeasurement { get; set; }
+
+        public string Message { get; set; }
+        public string Error { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            UnitOfMeasurement = await _context.UnitOfMeasurements.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (UnitOfMeasurement == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(UnitOfMeasurement).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UnitOfMeasurementExists(UnitOfMeasurement.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool UnitOfMeasurementExists(int id)
+        {
+            return _context.UnitOfMeasurements.Any(e => e.Id == id);
+        }
+    }
+}
