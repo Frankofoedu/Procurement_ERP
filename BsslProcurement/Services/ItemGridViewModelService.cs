97c20b4b006a88870c6a07fae1242b066fc1dda6
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-01-15 14:47:30 +0100

    added file view codes

--- BsslProcurement/Services/ItemGridViewModelService.cs
+++ BsslProcurement/Services/ItemGridViewModelService.cs
@@ -1,9 +1,12 @@
 ﻿using BsslProcurement.Interfaces;
 using BsslProcurement.ViewModels;
 using DcProcurement;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,16 +15,30 @@ namespace BsslProcurement.Services
     public class ItemGridViewModelService : IItemGridViewModelService
     {
         protected readonly ProcurementDBContext _procurementDBContext;
+        private readonly IWebHostEnvironment _Env;
 
-        public ItemGridViewModelService(ProcurementDBContext procurementDBContext)
+        public ItemGridViewModelService(ProcurementDBContext procurementDBContext, IWebHostEnvironment Env)
         {
             _procurementDBContext = procurementDBContext;
+            _Env = Env;
         }
         public Task<List<ItemGridViewModel>> GetItemsInRequisition(int requisitionId)
         {
-            return _procurementDBContext.RequisitionItems.Where(reItems => reItems.RequisitionId == requisitionId).Include(items => items.Attachment)
-                .Select(x=> new ItemGridViewModel { RequisitionItem = x })
+          var reqItems =  _procurementDBContext.RequisitionItems.Include(items => items.Attachment).Where(reItems => reItems.RequisitionId == requisitionId);
+              var items = reqItems.Select(x => new ItemGridViewModel { RequisitionItem = x, Attachment = x.Attachment == null ? null: GetFormFile(_Env.WebRootPath,x.Attachment.FilePath)})
                 .ToListAsync();
+
+            return items;
+        }
+
+        private static FormFile GetFormFile(string rootPath,string filePath)
+        {
+            using var stream = File.OpenRead(Path.Combine(rootPath, filePath));
+            return new FormFile(stream, 0, stream.Length, null, Path.GetFileName(stream.Name))
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "application/pdf"
+            };
         }
     }
 }
