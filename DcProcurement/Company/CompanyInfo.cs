8607202fa42ae5bd2b3067156500f227febd5c50
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-02-10 17:23:17 +0100

    updates

--- DcProcurement/Company/CompanyInfo.cs
+++ DcProcurement/Company/CompanyInfo.cs
@@ -15,11 +15,16 @@ namespace DcProcurement
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
+        public string Country { get; set; }
         public string Address { get; set; }
         public string PostalAddress { get; set; }
+        public string FaxNumber { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string NatureOfBusiness { get; set; }
+        public string OwnershipStructure { get; set; }
+        public string ParentCompanyName { get; set; }
+        public string CompanyWebsite { get; set; }
         public string Sector { get; set; }
         [DataType(DataType.Date)]
         public DateTime DateEstablishment { get; set; }
