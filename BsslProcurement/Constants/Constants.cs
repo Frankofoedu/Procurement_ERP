11f21e7f1fa8e7e9a1b3f76a0114bc2d7cd6d6f8
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-02-13 16:15:29 +0100

    added attachment constants

--- BsslProcurement/Constants/Constants.cs
+++ BsslProcurement/Constants/Constants.cs
@@ -3,15 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BsslProcurement.Constants
+namespace BsslProcurement.Const
 {
-    public class Constants
+    public static class ConstantVals
     {
-        public static List<SelectListItem> ProcurementMethod = new List<SelectListItem> {
+        public static readonly List<SelectListItem> ProcurementMethod = new List<SelectListItem> {
             new SelectListItem { Text = "DSP – Direct Shopping Procurement", Value = "1" },
             new SelectListItem { Text = "NSB – National Competitive Procurement", Value = "2" },
             new SelectListItem { Text = "ICB – International Competitive Procurement", Value = "3" },
             new SelectListItem { Text = "DSP – Direct Selective Procurement", Value = "4" },
         };
+
+        public const string AttachmentFolder = "Attachments";
     }
 }
