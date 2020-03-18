abd3aea9c80f94c85ae9b62be880d06cf0c31524
Obinna Okpara <obainoforgod@gmail.com>
2020-03-18 17:14:27 +0100

    View models for databinding

---
+++ BsslProcurement/ViewModels/DeclarationPartialInputViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BsslProcurement.ViewModels
+{
+    public class DeclarationPartialInputViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ BsslProcurement/ViewModels/GeneralInformationPartialViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BsslProcurement.ViewModels
+{
+    public class GeneralInformationPartialViewModel
+    {
+        public string OrganizationName { get; set; }
+        public string BrokerName { get; set; }
+        public string PreferredCurrency { get; set; }
+        public string NatureOfBusiness { get; set; }
+        public IFormFile LogoFile { get; set; }
+
+        public string FillerTitle { get; set; }
+        public string FillerLastName { get; set; }
+        public string FillerFirstName { get; set; }
+        public string FillerOtherName { get; set; }
+        public string FillerJobTitle { get; set; }
+        public string FillerDepartment { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
+        public string Email1 { get; set; }
+        public string Email2 { get; set; }
+        public string PhoneNumber1 { get; set; }
+        public string PhoneNumber2 { get; set; }
+        public string AccessLevel { get; set; }
+    }
+}
