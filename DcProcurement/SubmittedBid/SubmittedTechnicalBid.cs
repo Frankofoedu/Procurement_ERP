85d1a4e70fc9315017e05b205b5b0a5793938d4c
Obinna Okpara <obainoforgod@gmail.com>
2020-03-18 17:13:57 +0100

    New models for submitted bids

---
+++ DcProcurement/SubmittedBid/SubmittedTechnicalBid.cs
@@ -0,0 +1,14 @@
+﻿namespace DcProcurement
+{
+    public class SubmittedTechnicalBid
+    {
+        public int Id { get; set; }
+        public int? SubmittedBidId { get; set; }
+        public int? RequisitionItemId { get; set; }
+
+
+
+        public SubmittedBid SubmittedBid { get; set; }
+        public RequisitionItem RequisitionItem { get; set; }
+    }
+}
