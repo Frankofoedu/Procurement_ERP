9c152365a2ba9e4095f0d161d5fbe4322841ff81
Obinna Okpara <obainoforgod@gmail.com>
2020-03-18 17:11:16 +0100

    Update to bidprocess adding models to partial view

--- BsslProcurement/Pages/Vendor/BidProcess.cshtml.cs
+++ BsslProcurement/Pages/Vendor/BidProcess.cshtml.cs
@@ -8,9 +8,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BsslProcurement
 {
+
     [Authorize(AuthenticationSchemes = "Vendors")]
     public class BidProcessModel : PageModel
     {
+        [BindProperty]
+        public ViewModels.InvitePartialInputViewModel InviteInput { get; set; }
+
+        [BindProperty]
+        public ViewModels.DeclarationPartialInputViewModel DeclarationInput { get; set; }
+
+        [BindProperty]
+        public ViewModels.GeneralInformationPartialViewModel GeneralInput { get; set; }
+
         public void OnGet()
         {
 
