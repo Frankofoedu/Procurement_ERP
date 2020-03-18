cd00dd8504680660e84055bfd74f4957890c2cdd
Obinna Okpara <obainoforgod@gmail.com>
2020-03-18 12:24:59 +0100

    Added authorization for vendor and minor bug fixes

--- BsslProcurement/Pages/Vendor/BidProcess.cshtml.cs
+++ BsslProcurement/Pages/Vendor/BidProcess.cshtml.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BsslProcurement
 {
+    [Authorize(AuthenticationSchemes = "Vendors")]
     public class BidProcessModel : PageModel
     {
         public void OnGet()
