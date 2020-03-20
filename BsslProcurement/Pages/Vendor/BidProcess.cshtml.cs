bba78e85eff200aa1af8a0847dbe8d09cc349e02
Obinna Okpara <obainoforgod@gmail.com>
2020-03-20 17:45:20 +0100

    Bid process page updates

--- BsslProcurement/Pages/Vendor/BidProcess.cshtml.cs
+++ BsslProcurement/Pages/Vendor/BidProcess.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DcProcurement;
+using DcProcurement.Contexts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -21,9 +23,26 @@ namespace BsslProcurement
         [BindProperty]
         public ViewModels.GeneralInformationPartialViewModel GeneralInput { get; set; }
 
+        private readonly ProcurementDBContext _context;
+
+        public BidProcessModel(ProcurementDBContext context)
+        {
+            _context = context;
+
+        }
+
         public void OnGet()
         {
+            InviteInput = new ViewModels.InvitePartialInputViewModel();
+            DeclarationInput = new ViewModels.DeclarationPartialInputViewModel();
+            GeneralInput = new ViewModels.GeneralInformationPartialViewModel();
 
+
+        }
+
+        public void OnPost()
+        {
+            var m = DeclarationInput.Name;
         }
     }
 }
