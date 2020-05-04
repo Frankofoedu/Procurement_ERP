8b94878d913a05da816031518899422283cc3f75
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-05-04 08:11:05 +0100

    added group checking to <li> tags

---
+++ BsslProcurement/TagHelpers/SecureListTagHelper.cs
@@ -0,0 +1,51 @@
+﻿using BsslProcurement.AuthModels;
+using BsslProcurement.Filters;
+using DcProcurement;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BsslProcurement.TagHelpers
+{
+    [HtmlTargetElement("li", Attributes = "is-admin-control")]
+    public class SecureListTagHelper : TagHelper
+    {
+
+
+        private readonly DynamicAuthorizationOptions _authorizationOptions;
+
+        public SecureListTagHelper(DynamicAuthorizationOptions authorizationOptions) 
+        {
+
+            _authorizationOptions = authorizationOptions;
+        }
+
+
+
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+
+            var user = ViewContext.HttpContext.User;
+            if (!user.Identity.IsAuthenticated)
+            {
+                output.SuppressOutput();
+                return;
+            }
+
+            if (user.Identity.Name.Equals(_authorizationOptions.DefaultAdminUser, StringComparison.CurrentCultureIgnoreCase))
+                return;
+
+            output.SuppressOutput();
+        }
+
+    }
+}
