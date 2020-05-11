d8403a1010e56facbe5cd612558163419f0e4870
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-05-11 13:46:11 +0100

    added decrypt password

--- BsslProcurement/UtilityMethods/PasswordEncrypt.cs
+++ BsslProcurement/UtilityMethods/PasswordEncrypt.cs
@@ -12,5 +12,12 @@ namespace BsslProcurement.UtilityMethods
             byte[] passBytes = System.Text.Encoding.Unicode.GetBytes(pwd.ToUpper());
             return Convert.ToBase64String(passBytes);
         }
+
+        public static string GetDecryptedPassword(string base64pwd)
+        {
+            var byt = Convert.FromBase64String(base64pwd);
+            return  System.Text.Encoding.Unicode.GetString(byt);
+            
+        }
     }
 }
