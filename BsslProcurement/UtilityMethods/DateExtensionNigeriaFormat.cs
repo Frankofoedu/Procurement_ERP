fb0a7f30f3a4e38a4e81306a98b88f71270e9ca0
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-05-13 01:25:56 +0100

    nigerian date time format extension

---
+++ BsslProcurement/UtilityMethods/DateExtensionNigeriaFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BsslProcurement.UtilityMethods
+{
+    public static class DateExtensionNigeriaFormat
+    {
+        public static string ToShortNigeriaDateString(this DateTime date)
+        {
+            return date.ToString("dd/MM/yyyy hh:mm tt");
+        }
+    }
+}
