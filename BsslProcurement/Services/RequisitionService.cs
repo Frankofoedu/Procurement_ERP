641e30692af9d34a77c571bcf48b82ccd5882a47
Okpara Obinna <obainoforgod@gmail.com>
2020-06-02 00:49:14 +0100

    Added facility to create initiator job and send to quarantine, Changed when remark is made from when creating to when completed

--- BsslProcurement/Services/RequisitionService.cs
+++ BsslProcurement/Services/RequisitionService.cs
@@ -38,11 +38,11 @@ namespace BsslProcurement.Services
         }
         public async Task<List<Requisition>> GetRequisitionsForLoggedInUser(string userId)
         {
-            return await _procurementDBContext.Requisitions.Include(x => x.RequisitionItems).Where(p => p.LoggedInUserId == userId && p.isSubmitted == true).ToListAsync();
+            return await _procurementDBContext.Requisitions.Include(x => x.RequisitionItems).Where(p => p.LoggedInUserId == userId && p.RequisitionState == Enums.RequisitionState.Submitted).ToListAsync();
         }
         public async Task<List<Requisition>> GetSavedRequisitionsForLoggedInUser(string userId)
         {
-            return await _procurementDBContext.Requisitions.Include(x => x.RequisitionItems).ThenInclude(x=> x.Attachment).Where(p => p.LoggedInUserId == userId && p.isSubmitted == false).OrderBy(x => x.DateCreated).ToListAsync();
+            return await _procurementDBContext.Requisitions.Include(x => x.RequisitionItems).ThenInclude(x=> x.Attachment).Where(p => p.LoggedInUserId == userId && p.RequisitionState == Enums.RequisitionState.Saved).OrderBy(x => x.DateCreated).ToListAsync();
         }
         public async Task<List<Requisition>> GetBudgetClearedRequisitions()
         {
@@ -70,7 +70,7 @@ namespace BsslProcurement.Services
             if (oldReqJob != null)
             {
                 //update old requisitions jobs to done
-                oldReqJob.SetAsDone(DateTime.Now);
+                oldReqJob.SetAsDone(DateTime.Now, remark);
 
                 //checks if job is at final stage for that requistion
                 var maxWorkFlow = reqWorkFlow.Last();
@@ -82,7 +82,7 @@ namespace BsslProcurement.Services
 
                     if (requisition != null)
                     {
-                        requisition.isApproved = true;
+                        requisition.RequisitionState = Enums.RequisitionState.Approved;
                     }
                     
                 }
@@ -90,21 +90,35 @@ namespace BsslProcurement.Services
                 {
                     //send to next step
                     //create new job for next stage
-                    var newReqJob = new RequisitionJob(requisitionId, staffId, newWorkflowId, remark);
+                    var newReqJob = new RequisitionJob(requisitionId, staffId, newWorkflowId);
                     _procurementDBContext.RequisitionJobs.Add(newReqJob);
                 }
 
             }
             else
             {
-
                 //create new job for next stage
-                var newReqJob = new RequisitionJob(requisitionId, staffId, newWorkflowId, remark);
+                var newReqJob = new RequisitionJob(requisitionId, staffId, newWorkflowId);
                 _procurementDBContext.RequisitionJobs.Add(newReqJob);
             }
 
             await _procurementDBContext.SaveChangesAsync();
 
+        }
+
+        //Create initiator job
+        public async Task CreateInitiatorJobAsync(int requisitionId, string staffId, string remark)
+        {
+            //get Initiator workflow
+            var InitiatorWorkflow = await _procurementDBContext.Workflows.Include(m=>m.WorkflowAction).Include(n=>n.WorkflowType)
+                .FirstOrDefaultAsync(IW => IW.WorkflowType.Name == Constants.RequisitionWorkflow && IW.WorkflowAction.Name == Constants.RequisitionInitiatorActionName);
+
+            var newReq = new RequisitionJob(requisitionId, staffId, InitiatorWorkflow.Id);
+            newReq.SetAsDone(DateTime.Now, remark);
+
+            _procurementDBContext.RequisitionJobs.Add(newReq);
+
+            await _procurementDBContext.SaveChangesAsync();
         }
 
         //TODO: Update this method in line with the nextstage method
@@ -123,13 +137,33 @@ namespace BsslProcurement.Services
                 throw new ArgumentNullException("Job does not exist");
             }
             //set current job as cancelled
-            currJob.SetAsCancelled(DateTime.Now);
+            currJob.SetAsCancelled(DateTime.Now, remark);
 
 
             //create new job for previous stage
-            var newReqJob = new RequisitionJob(requisitionId, newStaffId, newStage, remark);
+            var newReqJob = new RequisitionJob(requisitionId, newStaffId, newStage);
 
             _procurementDBContext.Add(newReqJob);
+
+            await _procurementDBContext.SaveChangesAsync();
+        }
+
+        public async Task SendToQuarantine(int requisitionId, string remark)
+        {
+            //get current job
+            var currJob = await _procurementDBContext.RequisitionJobs.FirstOrDefaultAsync(x => x.JobStatus == Enums.JobState.NotDone && x.RequisitionId == requisitionId);
+
+            if (currJob == null)
+            {
+                throw new ArgumentNullException("Job does not exist");
+            }
+
+            //set current job as done
+            currJob.SetAsDone(DateTime.Now, remark);
+
+            var req = await _procurementDBContext.Requisitions.FirstOrDefaultAsync(m => m.Id == requisitionId);
+            req.RequisitionState = Enums.RequisitionState.Quarantined;
+            req.Status = "Quarantined";
 
             await _procurementDBContext.SaveChangesAsync();
         }
