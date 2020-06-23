87450f80782f7b44a3e27ad9ad709e5cdf7497f4
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-06-23 15:04:23 +0100

    fixes #54. @Knwago Please confirm

--- BsslProcurement/ViewModels/ItemGridViewModel.cs
+++ BsslProcurement/ViewModels/ItemGridViewModel.cs
@@ -8,7 +8,8 @@ namespace BsslProcurement.ViewModels
 {
    public class ItemGridViewModel
     {
-
+        //used for the grid view
+        public int Index { get; set; }
         public RequisitionItem RequisitionItem { get; set; }
         public int? AttachmentId { get; set; }
         public IFormFile Attachment { get; set; }
