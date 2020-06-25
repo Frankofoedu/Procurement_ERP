af05737d125eda5da1d31c073137c92e106acee0
Okpara Obinna <obainoforgod@gmail.com>
2020-06-25 14:46:50 +0100

    PR Number service Interface

---
+++ BsslProcurement/Interfaces/IPRNumberService.cs
@@ -0,0 +1,15 @@
+﻿using BsslProcurement.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BsslProcurement.Interfaces
+{
+    public interface IPRNumberService
+    {
+        Task<string> GetNewPRNumberAsync(string DashedPRno);
+        Task<PRNumberServiceDataViewModel> GetDashedPRNumberAndDepartmentDataAsync(string userCode);
+    }
+}
