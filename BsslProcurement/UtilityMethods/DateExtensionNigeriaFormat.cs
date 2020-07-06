8c3522ffd0c7294908f0783fc34f2780c7f77d8b
Okpara Obinna <obainoforgod@gmail.com>
2020-07-06 17:19:02 +0100

    Added a new string extension for converting date string to datetime.

--- BsslProcurement/UtilityMethods/DateExtensionNigeriaFormat.cs
+++ BsslProcurement/UtilityMethods/DateExtensionNigeriaFormat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,25 @@ namespace BsslProcurement.UtilityMethods
     {
         public static string ToShortNigeriaDateString(this DateTime date)
         {
-            return date.ToString("dd/MM/yyyy hh:mm tt");
+            //return date.ToString("dd/MM/yyyy hh:mm tt");
+            return date.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+        }
+
+        public static DateTime ToNigeriaDate(this string date)
+        {
+            try
+            {
+                var dateonly = date.Split(' ').First();
+                var splitDate = dateonly.Split('/');
+
+                var dDate = new DateTime(int.Parse(splitDate[2], CultureInfo.InvariantCulture), int.Parse(splitDate[1], CultureInfo.InvariantCulture), int.Parse(splitDate[0], CultureInfo.InvariantCulture));
+
+                return dDate;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
