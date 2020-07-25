4848943fe66c0d068947b455dfaaa6872ddcf86c
Okpara Obinna <38779294+ObinnaOkpara@users.noreply.github.com>
2020-07-25 04:39:58 +0100

    Create ModelInput.cs

---
+++ BsslProcurement/AuthModels/ML/ModelInput.cs
@@ -0,0 +1,19 @@
+  
+using System;
+using Microsoft.ML;
+
+namespace BsslProcurement
+{
+public class ModelInput
+    {
+        [ColumnName("Label"), LoadColumn(0)]
+        public string Label { get; set; }
+
+
+        [ColumnName("ImageSource"), LoadColumn(1)]
+        public string ImageSource { get; set; }
+
+
+    }
+
+}
